8bdb51c31ccdd9b6ec2088c6573e53524bfc8bcf
creeper88888 <92006453+creeper88888@users.noreply.github.com>
2023-10-16 10:49:49 +0800

    Update 1.insert.php

--- 1.insert.php
+++ 1.insert.php
@@ -5,7 +5,6 @@ $jobName=$_POST['name']; //$_GET, $_REQUEST
 $jobUrgent=$_POST['urgent'];
 $jobContent=$_POST['content'];
 $jobState=$_POST['state'];
-
 	$sql = "insert into todo (jobName, jobUrgent, jobContent, jobState) values (?, ?, ?, ?)"; //SQL中的 ? 代表未來要用變數綁定進去的地方
 	$stmt = mysqli_prepare($db, $sql); //prepare sql statement
 	mysqli_stmt_bind_param($stmt, "ssss", $jobName, $jobUrgent,$jobContent,$jobState); //bind parameters with variables, with types "sss":string, string ,string
